f12082261a58a98b5e57f9bf5245995f6976412f
amnon-vana <52174507+amnon-vana@users.noreply.github.com>
2019-07-01 17:55:15 +0300

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -2,3 +2,14 @@ matrix = [[5,5,5],[1,-9,44],[-20,10,5]]
 sum = 0
 for num in matrix[0]:
   sum += num
+min_sum_num = sum
+min_sum_list = matrix[0]
+
+for mylist in matrix[1:]:
+  sum = 0
+  for num in mylist:
+    sum += num
+  if sum < min_sum_num:
+    min_sum_num = sum
+    min_sum_list = mylist
+print("Min sum is: {} of list {}".format(min_sum_num,min_sum_list))
