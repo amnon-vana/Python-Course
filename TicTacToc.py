af1837dc330aeed397f434f733a4228a75d24643
amnon-vana <52174507+amnon-vana@users.noreply.github.com>
2019-07-08 20:40:23 +0300

    Create TicTacToc.py
    First project

---
+++ TicTacToc.py
@@ -0,0 +1,20 @@
+def checkRowWin(row,sign):
+    if matrix[row][0] == sign and matrix[row][1] == sign and matrix[row][2] == sign:
+        return True
+    return False
+
+def checkColWin(col,sign):
+    if matrix[0][col] == sign and matrix[1][col] == sign and matrix[2][col] == sign:
+         return True
+    return False
+
+def checkLegalPosition(row,col,sign):
+    
+
+
+res = checkRowWin(1,"x")
+print(f"Result is {res}")
+
+res = checkColWin(1,"x")
+print(f"Result is {res}")
+
